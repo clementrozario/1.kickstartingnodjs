b8915141f124cef064ecfdf6946dd1be8ce40860
clementrozario <clementcla@outlook.com>
2023-07-22 19:43:49 +0530

    some function not working properlyy

--- readfile.js
+++ readfile.js
@@ -15,7 +15,9 @@ const requestHandler = (req, res) => {
       const parsedBody = Buffer.concat(body).toString();
       console.log("parsedBody>>", parsedBody);
       const message = parsedBody.split("=")[1];
-      fs.writeFile("message.txt", message, (err) => {
+
+      // Append the new message to the file
+      fs.appendFile("message.txt", message + "\n", (err) => {
         if (err) {
           console.log(err);
           return;
@@ -27,17 +29,39 @@ const requestHandler = (req, res) => {
       });
     });
   } else {
-    res.setHeader("Content-Type", "text/html");
-    res.write("<html>");
-    res.write("<head><title>EnterMessage</title></head>");
-    res.write("<body>");
-    res.write('<form action="/message" method="POST">');
-    res.write('<input type="text" name="message">');
-    res.write('<button type="submit">Send</button>');
-    res.write("</form>");
-    res.write("</body>");
-    res.write("</html>");
-    return res.end();
+    // Read all messages from the file
+    fs.readFile("message.txt", { encoding: "utf-8" }, (err, data) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+
+      const messages = data.split("\n").filter((msg) => msg.trim() !== "");
+
+      res.setHeader("Content-Type", "text/html");
+      res.write("<html>");
+      res.write("<head><title>EnterMessage</title></head>");
+      res.write("<body>");
+      res.write('<form action="/message" method="POST">');
+      res.write('<input type="text" name="message">');
+      res.write('<button type="submit">Send</button>');
+      res.write("</form>");
+
+      // Display existing messages
+      if (messages.length > 0) {
+        res.write("<ul>");
+        messages.forEach((msg) => {
+          res.write(`<li>${msg}</li>`);
+        });
+        res.write("</ul>");
+      } else {
+        res.write("<p>No messages yet.</p>");
+      }
+
+      res.write("</body>");
+      res.write("</html>");
+      return res.end();
+    });
   }
 };
 
