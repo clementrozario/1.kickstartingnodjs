958f8028ae3ca61245288f489c00719f0c566303
clementrozario <clementcla@outlook.com>
2023-07-25 16:08:17 +0530

    routers and filters

--- index.js
+++ index.js
@@ -2,21 +2,18 @@ const express=require('express')
 const bodyParser=require('body-parser');
 
 const app=express();
-app.use(bodyParser.urlencoded());
 
+const adminRoutes = require('./routes/admin')
+const shopRoutes =require('./routes/shop');
 
-app.use('/add-product',(req,res,next)=>{
-    res.send('<form action="/product" method="POST"><input type="text" name="title"> <input type="text" name="size"><button type="sumbit">Add Product</button></form>'
-    
-    )});
+app.use(bodyParser.urlencoded({extended:false}));
 
-app.use('/product',(req,res,next)=>{
-    console.log(req.body);
-    res.redirect('/')
-});
+app.use('/admin',adminRoutes);
+app.use(shopRoutes);
+ 
+app.use((req,res,next)=> {
+    res.status(404).send('<h1>Page not found</h1>')
 
-app.use('/',(req,res,next)=>{
-    res.send('<h1>Hello From Express!</h1>');
 });
 
 app.listen(3000);
